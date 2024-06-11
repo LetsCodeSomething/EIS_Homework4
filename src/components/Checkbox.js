bbc112c9b73a0c3089542ee1c739a2d7d7ad3580
LetsCodeSomething <mishaLySL@yandex.ru>
2024-06-11 12:39:27 +1000

    Remove unnecessary code from checkbox

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,18 +4,10 @@ export function Checkbox(props) {
     const onChange = (event) => {
         props.updateFilters(props.filterName, event.target.checked, props.index);
     };
-
-    if(props.value) {
-        return(
-            <>
-                <input type="checkbox" onChange={onChange} checked disabled={props.disabled}/>
-            </>
-        );
-    }
     
     return(
         <>
-            <input type="checkbox" onChange={onChange} disabled={props.disabled}/>
+            <input type="checkbox" onChange={onChange} checked={props.value} disabled={props.disabled}/>
         </>
     );
 }
